cb87e29f4ad1d4fa7bf8d011e710b6279fc69b59
Rudenko Artem <artem.rud@bk.ru>
2019-11-25 01:56:14 +0200

    Updates

---
+++ precache-manifest.027970f7e6c529768b09ba63efbd3f2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ffef3d350fd203e7d8aa9d42db3d7b",
+    "url": "/UberEatsTemp/index.html"
+  },
+  {
+    "revision": "4be80cf4bf6efca3183e",
+    "url": "/UberEatsTemp/static/css/main.4300d67e.chunk.css"
+  },
+  {
+    "revision": "a239e4da9b6d14aad580",
+    "url": "/UberEatsTemp/static/js/2.7e9954d6.chunk.js"
+  },
+  {
+    "revision": "4be80cf4bf6efca3183e",
+    "url": "/UberEatsTemp/static/js/main.96adc5bf.chunk.js"
+  },
+  {
+    "revision": "b1ea46256f71f9f1d5ce",
+    "url": "/UberEatsTemp/static/js/runtime~main.25cae0e7.js"
+  }
+]);
